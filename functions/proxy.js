fc6b73c1f8061f05d06c15e08b4a70e4271c548a
Raggooon <144166079+Raggooon@users.noreply.github.com>
2024-03-05 09:32:17 -0500

    Update proxy.js

--- functions/proxy.js
+++ functions/proxy.js
@@ -1,1 +1,19 @@
+const fetch = require('node-fetch');
+
+exports.handler = async function(event) {
+    const { url } = event.queryStringParameters;
+    try {
+        const response = await fetch(url);
+        const data = await response.text();
+        return {
+            statusCode: 200,
+            body: data
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: 'Server Error: ' + error.message
+        };
+    }
+};
 
